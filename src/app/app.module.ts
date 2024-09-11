4ef4ede7a2b0cde73f67867c7951a63cc35fbd0b
sonvv <sonvv@rikkeisoft.com>
2024-09-11 18:27:28 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutHomeComponent } from './core/layout/layout-home/layout-home.component';
+import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutHomeComponent
+    LayoutHomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
